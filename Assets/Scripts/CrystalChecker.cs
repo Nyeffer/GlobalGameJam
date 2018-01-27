a5b4f2b48da2e0185eaacdb24e547f73ac94f341
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-01-26 20:33:29 -0500

    CrystalChecker Works

---
+++ Assets/Scripts/CrystalChecker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrystalChecker : MonoBehaviour {
+
+	public int requiredAmtOfCrystal = 0;
+	private int playerAmtofCrystal;
+	public GameObject TextPanel;
+	public Text howManyCrystalLeft;
+
+	void Awake() {
+		TextPanel.SetActive(false);
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if(col.gameObject.tag == "PlayerInteract") {
+			int numOfCrystals = col.GetComponent<GatherCrystal>().GetNumOfCrystal();
+			if(numOfCrystals >= requiredAmtOfCrystal) {
+				gameObject.SetActive(false);
+			} else {
+				TextPanel.SetActive(true);
+				howManyCrystalLeft.text = "You Need " + (requiredAmtOfCrystal - numOfCrystals).ToString() + " More Crystals";
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		if(col.gameObject.tag == "PlayerInteract") {
+			TextPanel.SetActive(false);
+		}
+
+	}
+}
