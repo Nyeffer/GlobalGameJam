8799369de6b3aa65aaf992cf5f9e89414ce5ab74
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-01-27 04:24:02 -0500

    Tutorial Done Begin To End

---
+++ Assets/Scripts/ConstantRot.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRot : MonoBehaviour {
+
+	public float rotSpeed = 150.0f;
+
+	void Update() {
+		gameObject.transform.Rotate(0, rotSpeed * Time.deltaTime, 0);
+	}
+}
---
+++ Assets/Scripts/Tutorial_Done.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tutorial_Done : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col) {
+		if(col.gameObject.tag == "Player") {
+			SceneManager.LoadScene("Tutorial", LoadSceneMode.Single);
+		}
+	}
+}
