bdb3e7c34d7927530dc086d86b939efd75b4183f
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-01-26 19:51:21 -0500

    Gathering Main Resource Done

---
+++ Assets/Scripts/GatherCrystal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GatherCrystal : MonoBehaviour {
+
+	public Text text_Crystal;
+
+	private int numOfCrystal = 0;
+	
+	void Update() {
+		text_Crystal.text = GetNumOfCrystal().ToString();
+	}
+
+	void OnTriggerEnter(Collider col) {
+		Debug.Log("Got it");
+		if(col.gameObject.tag == "Crystal") {
+			numOfCrystal++;
+			Destroy(col.gameObject);
+		}
+	}
+
+	public int GetNumOfCrystal() {
+		return numOfCrystal;
+	}
+
+}
---
+++ Assets/Scripts/SetUpPlayerPrefs.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetUpPlayerPrefs : MonoBehaviour {
+
+	private int curHealth;
+	private int curNumCrytals;
+
+	void Start() {
+		curNumCrytals = PlayerPrefs.GetInt("CurrentNumberOfCrystals");
+		if(curNumCrytals <= 0) {
+			PlayerPrefs.SetInt("CurrentNumberOfCrystals", 0);
+		}
+	}
+
+}
