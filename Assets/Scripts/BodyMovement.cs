7041316cc07c41d6fd4fdbe0375cf229c579c480
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-01-27 10:57:07 -0500

    Made Player Model

---
+++ Assets/Scripts/BodyMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyMovement : MonoBehaviour {
+
+	public GameObject r_Arm;
+	public GameObject l_Arm;
+	public GameObject r_Leg;
+	public GameObject l_Leg;
+
+	
+}
