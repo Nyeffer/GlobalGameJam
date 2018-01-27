985db2b3919f8b347992bb517bca852918aa7bc2
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-01-26 23:04:18 -0500

    Spawner Works

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+	public GameObject Enemy;
+	float counter;
+
+	void Start() {
+		counter = 0;
+	}
+
+	void Update() {
+		counter += Time.deltaTime;
+		if (counter >= 5) {
+			Instantiate(Enemy, gameObject.transform.position, Quaternion.identity);
+			counter = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/SpawnTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrigger : MonoBehaviour {
+
+	public GameObject Spawner;
+
+	void Awake() {
+		Spawner.SetActive(false);
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if(col.gameObject.tag == "Player") {
+			Spawner.SetActive(true);
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		if(col.gameObject.tag == "Player") {
+			Spawner.SetActive(false);
+		}
+	}
+
+
+}
